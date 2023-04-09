548bf9d5dbc793bd0ab488d2c548cfedd685215b
Inanis lector <kid@kde.kiev.ua>
2023-04-09 16:13:50 +0300

    Start of the game
    Added DLLs for color in console
    Created game loop
    Started writing map
    Started writing Vector2 and Quaternion rotationfor it

---
+++ SeaBattle/SeaBattle/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    /*
+         *  "\x1b[48;5;" + s + "m" - set background color by index in table (0-255)
+         *
+         *  "\x1b[38;5;" + s + "m" - set foreground color by index in table (0-255)
+         *
+         *  "\x1b[48;2;" + r + ";" + g + ";" + b + "m" - set background by r,g,b values
+         *
+         *  "\x1b[38;2;" + r + ";" + g + ";" + b + "m" - set foreground by r,g,b values
+         * 
+         */
+
+    #region DLL
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool GetConsoleMode(IntPtr handle, out int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr GetStdHandle(int handle);
+    
+    #endregion
+
+    static void Main(string[] args)
+    {
+        var handle = GetStdHandle(-11);
+        int mode;
+        GetConsoleMode(handle, out mode);
+        SetConsoleMode(handle, mode | 0x4);
+
+        while (true)
+        {
+            SeaBattle game = new();
+            game.Start();
+        }
+    }
+}
+
+class SeaBattle
+{
+    public void Start()
+    {
+        Init();
+
+        GameCycle();
+    }
+
+    private void Init()
+    {
+
+    }
+
+    private void GameCycle()
+    {
+        while (!CheckForWin())
+        {
+            Render();
+        }
+    }
+
+    private bool CheckForWin()
+    {
+        return false;
+    }
+
+    private void Render()
+    {
+
+    }
+}
+
+struct Map
+{
+    private bool[,] _shipMap;
+    private bool[,] _shotMap;
+
+    public Map()
+    {
+    _shipMap = new bool[8, 8];
+    _shotMap = new bool[8, 8];
+}
+
+    enum mapType
+    {
+        shot,
+        ship
+    }
+
+    public (bool ship, bool shot) this[int x, int y]
+        => (_shipMap[x, y], _shotMap[x, y]);
+
+    public (bool ship, bool shot) this[int x, int y]
+        => (_shipMap[x, y], _shotMap[x, y]);
+}
+
+struct Vector2
+{
+    public int x;
+    public int y;
+
+    public static Vector2 Zero => new Vector2(0, 0);
+    public static Vector2 Up => new Vector2(0, -1);
+    public static Vector2 Down => new Vector2(0, 1);
+    public static Vector2 Left => new Vector2(-1, 0);
+    public static Vector2 Right => new Vector2(1, 0);
+
+
+    public Vector2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        => new Vector2(v1.x + v2.x, v1.y + v2.y);
+
+    public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        => new Vector2(v1.x - v2.x, v1.y - v2.y);
+
+    public static Vector2 operator *(Vector2 v1, int multiplier)
+        => new Vector2(v1.x * multiplier, v1.y * multiplier);
+
+    public static bool operator ==(Vector2 v1, Vector2 v2)
+        => v1.x == v2.x && v1.y == v2.y;
+
+    public static bool operator !=(Vector2 v1, Vector2 v2)
+        => !(v1 == v2);
+
+    public override string ToString()
+        => $"({x}, {y})";
+
+    public void Deconstruct(out int x, out int y)
+    {
+        x = this.x;
+        y = this.y;
+    }
+}
